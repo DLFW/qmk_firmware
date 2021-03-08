4af75fade345817ed3814d65bcd323c77ee9b43b
Daniel Llin Ferrero <daniel@llin.info>
2021-03-08 21:59:28 +0100

    enable underglow and split features

--- keyboards/terminal_b/config.h
+++ keyboards/terminal_b/config.h
@@ -62,9 +62,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define BACKLIGHT_LEVELS 3
 //#define BACKLIGHT_BREATHING
 
-//#define RGB_DI_PIN E2
+#define RGB_DI_PIN D2
 //#ifdef RGB_DI_PIN
-//#    define RGBLED_NUM 16
+#define RGBLED_NUM 16
 //#    define RGBLIGHT_HUE_STEP 8
 //#    define RGBLIGHT_SAT_STEP 8
 //#    define RGBLIGHT_VAL_STEP 8
