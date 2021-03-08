0850d29b2582a4d9a97bae3ca2a8f0b5f67fa24d
Daniel Llin Ferrero <daniel@llin.info>
2021-03-08 22:38:36 +0100

    adapt switch matrix

--- keyboards/terminal_b/config.h
+++ keyboards/terminal_b/config.h
@@ -27,8 +27,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT      terminal_b
 
 /* key matrix size */
-#define MATRIX_ROWS 2
-#define MATRIX_COLS 3
+#define MATRIX_ROWS 10  // 2 * 5 for each split half; split boards are "merged" by their rows
+#define MATRIX_COLS 6
 
 /*
  * Keyboard Matrix Assignments
@@ -40,8 +40,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_ROW_PINS { D0, D5 }
-#define MATRIX_COL_PINS { F1, F0, B0 }
+#define MATRIX_ROW_PINS { C6, D7, E6, B4, B5 }
+#define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL */
--- keyboards/terminal_b/terminal_b.h
+++ keyboards/terminal_b/terminal_b.h
@@ -26,10 +26,23 @@
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
  */
+
 #define LAYOUT( \
-    k00, k01, k02, \
-      k10,  k12    \
-) { \
-    { k00, k01,   k02 }, \
-    { k10, KC_NO, k12 }  \
+    L00, L01, L02, L03, L04, L05,    R00, R01, R02, R03, R04, R05, \
+    L10, L11, L12, L13, L14, L15,    R10, R11, R12, R13, R14, R15, \
+    L20, L21, L22, L23, L24, L25,    R20, R21, R22, R23, R24, R25, \
+    L30, L31, L32,                                  R30, R31, R32, \
+    L40, L41, L42, L43, L44, L45,    R40, R41, R42, R43, R44, R45  \
+) \
+{ \
+    {L00,   L01,   L02,   L03,   L04,   L05  }, \
+    {L10,   L11,   L12,   L13,   L14,   L15  }, \
+    {L20,   L21,   L22,   L23,   L24,   L25  }, \
+    {L30,   L31,   L32,   KC_NO, KC_NO, KC_NO}, \
+    {L40,   L41,   L42,   L43,   L44,   L45  }, \
+    {R00,   R01,   R02,   R03,   R04,   R05  }, \
+    {R10,   R11,   R12,   R13,   R14,   R15  }, \
+    {R20,   R21,   R22,   R23,   R24,   R25  }, \
+    {KC_NO, KC_NO, KC_NO, R30,   R31,   R32  }, \
+    {R40,   R41,   R42,   R43,   R44,   R45  }  \
 }
