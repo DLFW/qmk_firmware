9c33a2680c7b2e437999b0da6a2eaf7e04f0f404
Daniel Llin Ferrero <daniel@llin.info>
2021-04-06 22:18:02 +0200

    add second layer

--- keyboards/terminal_b/keymaps/default/keymap.c
+++ keyboards/terminal_b/keymaps/default/keymap.c
@@ -17,8 +17,15 @@
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
-    _BASE
+    BASE,
+    COMP,
+    NAVI
 };
+
+#define T_COMP TT(BASE)
+#define T_NAVI TT(NAVI)
+#define C_LEFT MT(MOD_LCTL, KC_DEL)
+#define C_RIGT MT(MOD_RCTL, KC_BSPC)
 
 // Defines the keycodes used by our macros in process_record_user
 enum custom_keycodes {
@@ -28,13 +35,21 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
-    [_BASE] = LAYOUT(
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,        KC_Y,      KC_U,     KC_I,    KC_O,    KC_P,    KC_EQL,  \
-        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,     KC_G,        KC_H,      KC_J,     KC_K,    KC_L,    KC_NUHS, KC_RBRC, \
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,        KC_N,      KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-        KC_LCTL, KC_CAPS, KC_LALT,                                                               KC_LALT, KC_CAPS, KC_RCTL, \
-                          KC_1,    KC_2,    KC_SPC,   KC_3,        KC_4,      KC_ENT,   KC_5,    KC_6,                      \
-                                   KC_7,    KC_8,                             KC_9,     KC_0                                \
+    [BASE] = LAYOUT(
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,  \
+        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_NUHS, KC_RBRC, \
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+        KC_RALT, KC_NO,   KC_NO,                                                             KC_NO,   KC_NO,   KC_RALT, \
+                          KC_LGUI, T_COMP,  KC_SPC,  T_NAVI,      T_NAVI,  KC_ENT,  T_COMP,  KC_RGUI,                   \
+                                   C_LEFT,  KC_LALT,                       KC_LALT, C_RIGT                              \
+    ),
+    [NAVI] = LAYOUT(
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
+        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12,  \
+        _______, KC_APP,  KC_NO,   KC_GRV,  KC_NUBS, KC_DEL,      KC_BSPC, KC_QUOT, KC_SCLN, KC_LBRC, KC_MINS, _______, \
+        _______, _______, _______,                                                           _______, _______, _______, \
+                          _______, _______, _______, _______,     _______, _______, _______, _______,                   \
+                                   _______, _______,                       _______, _______                             \
     )
 };
 
