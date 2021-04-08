cc6d82af4637f44f355fc3d58f412dbfac96f8a2
Daniel Llin Ferrero <daniel@llin.info>
2021-04-08 18:36:42 +0200

    enable swap-hands

--- keyboards/terminal_b/keymaps/default/keymap.c
+++ keyboards/terminal_b/keymaps/default/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,  \
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_NUHS, KC_RBRC, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-        KC_RALT, XXXXXXX, XXXXXXX,                                                           XXXXXXX, XXXXXXX, KC_RALT, \
+        KC_RALT, SH_TT,   XXXXXXX,                                                             XXXXXXX, SH_TT, KC_RALT, \
                           KC_LGUI, T_COMP,  KC_SPC,  T_NAVI,      T_NAVI,  KC_ENT,  T_COMP,  KC_RGUI,                   \
                                    C_LEFT,  KC_LALT,                       KC_LALT, C_RIGT                              \
     ),
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                    _______, _______,                       _______, _______                             \
     ),
     [META] = LAYOUT(
-        EEP_RST, RESET, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RESET,   EEP_RST, \
+        EEP_RST, RESET, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RESET,   EEP_RST, \
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, T_BASE,      T_BASE,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
         XXXXXXX, XXXXXXX, XXXXXXX,                                                           XXXXXXX, XXXXXXX, XXXXXXX, \
