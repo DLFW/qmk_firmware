e543fb34fda8c2f20c97bc6c9c775392b4e0e907
Daniel Llin Ferrero <daniel@llin.info>
2021-04-26 17:44:47 +0200

    keymap improvements

--- keyboards/terminal_b/keymaps/default/keymap.c
+++ keyboards/terminal_b/keymaps/default/keymap.c
@@ -81,7 +81,7 @@ enum custom_keycodes {
 //#define A_LEFT MT(MOD_LALT, KC_DEL)   // KC_LALT
 //#define A_RIGT MT(MOD_LALT, KC_BSPC)  // KC_LALT
 #define MT_RALT MT(MOD_RALT, KC_DEL)
-#define MT_LALT MT(MOD_LALT, KC_BSPC)
+#define MT_LALT MT(MOD_RALT, KC_BSPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
@@ -89,14 +89,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,  \
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_NUHS, KC_RBRC, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-        KC_LCTL, SH_TT,   KC_LEAD,                                                           KC_LEAD, SH_TT,   KC_RCTL, \
+        KC_LCTL, KC_LALT, SH_TT,                                                             SH_TT,   KC_LALT, KC_RCTL, \
                           KC_LGUI, KC_SPC,  T_COMP,  T_NAVI,     T_NAVI,   T_COMP,  KC_ENT,  KC_RGUI,                   \
                                    KC_LEAD, MT_RALT,                       MT_LALT, KC_LEAD                             \
     ),
     [COMP] = LAYOUT(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
         _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12,  \
-        _______, KC_APP,  KC_NO,   KC_GRV,  KC_NUBS, KC_DEL,      KC_BSPC, KC_QUOT, KC_SCLN, KC_LBRC, KC_MINS, _______, \
+        _______, KC_APP,  KC_PSCR, KC_GRV,  KC_NUBS, KC_DEL,      KC_BSPC, KC_QUOT, KC_SCLN, KC_LBRC, KC_MINS, _______, \
         _______, _______, _______,                                                           _______, _______, _______, \
                           _______, _______, _______, _______,     _______, _______, _______, _______,                   \
                                    _______, _______,                       _______, _______                             \
